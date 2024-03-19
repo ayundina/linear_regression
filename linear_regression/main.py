9fd88d7f0599c0f5a267cdb344339a006d49b75b
Anna Yundina <anna_yundina@Annas-MacBook-Pro.local>
2024-03-19 14:54:47 +0100

    compare to sklearn LinearRegression

--- linear_regression/main.py
+++ linear_regression/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn import datasets
+from sklearn.linear_model import LinearRegression
 
 from train import train
 from predict import predict
@@ -8,8 +9,7 @@ from visualise import visualise_prediction
 
 
 def get_dataset(filename):
-    data = np.genfromtxt(filename, unpack=True,
-                         delimiter=',', skip_header=True).T
+    data = np.genfromtxt(filename, unpack=True, delimiter=',', skip_header=True).T
     X = data[:, 0].reshape(-1, 1)
     y = data[:, 1]
     return X, y
@@ -22,15 +22,26 @@ def generate_dataset():
 
 
 def main():
-    X, y = get_dataset("../data.csv")
+    # X, y = get_dataset("../data.csv")
+    X, y = get_dataset("../ex1data1.txt")
     # X, y = generate_dataset()
     visualise_scatter(X.reshape(-1), y, "data_raw")
     model = train(X, y)
     y_prediction_line = predict(model, X)
-    destandardized_error = model.mean_squared_error(X, y)
-    print(f"DEstandardized_error = {destandardized_error}")
+    print(f"weights = {model.weights}")
+    print(f"bias = {model.bias}")
+    print(f"score = {model.score(X, y)}")
     visualise_prediction(X.reshape(-1), y, y_prediction_line, "data_trained")
+
+    model = LinearRegression()
+    model.fit(X, y)
+    print(f"score = {model.score(X, y)}")
+    print(f"weights = {model.coef_}")
+    print(f"bias = {model.intercept_}")
+    visualise_prediction(X.reshape(-1), y, model.predict(X), "data_skl_trained")
 
 
 if __name__ == "__main__":
     main()
+
+# next step: implement for multivariate
